c9ff984df4b0bcc93ad439ced36c099e6f2fb1fe
nithyabz <vnitya@beingzero.in>
2023-02-13 22:26:02 +0530

    added html

--- server.js
+++ server.js
@@ -3,10 +3,9 @@ const app = express();
 const port = process.env.PORT || 5010;
 
 app.get("/", function(req, res){
-	res.send("Hello World, I am Nithya");
+    // res.send("Hi Nithya");
+    res.sendFile(__dirname + "/index.html");
 });
-
 app.listen(port, function(){
-	console.log("Server running on http://localhost:"+port);
-	console.log(`Server running on http://localhost:${port}`);
+    console.log("Server is running on http://localhost:" + port);
 });
