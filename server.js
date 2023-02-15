0ab41654ddcb97a73427624129d859821a6ff0b0
nithyabz <vnitya@beingzero.in>
2023-02-15 22:03:58 +0530

    added database

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5010;
@@ -10,6 +12,25 @@ app.get("/resume", function(req, res){
     // res.send("Hi Nithya");
     res.sendFile(__dirname + "/resume.html");
 });
-app.listen(port, function(){
-    console.log("Server is running on http://localhost:" + port);
+app.get("/cssDemo", function(req, res){
+    // res.send("Hi Nithya");
+    res.sendFile(__dirname + "/resumeClass.html");
+});
+app.get("/cards", function(req, res){
+    // res.send("Hi Nithya");
+    res.sendFile(__dirname + "/cardDemo.html");
+});
+mongoose.set('strictQuery',true);
+mongoose.connect(process.env.MONGO_CONNECTION_STRING,{},function(err){
+   if(err)
+   {
+    console.error(err);
+   } 
+   else
+   {
+    console.log("DB CONNECTED");
+    app.listen(port, function(){
+        console.log("Server is running on http://localhost:" + port);
+    });
+   }
 });
