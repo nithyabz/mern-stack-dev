5218dde1beac4c4f7af790b5ee6dfb6f44c49fc2
nithyabz <vnitya@beingzero.in>
2023-02-18 20:24:00 +0530

    added weather page

--- backend/lib/userLib.js
+++ backend/lib/userLib.js
@@ -28,3 +28,50 @@ module.exports.createFirstUser = async function(callback){
         callback(err, null);
     }
 }
+module.exports.createUser = async function(user,callback){
+    try{
+        var newUser = new userModel(user);
+        var result = await newUser.save();
+        callback(null,result);
+    }
+    catch(err){
+        callback(err,null);
+    }
+}
+
+module.exports.updateUser = async function(username,data,callback){
+    try{
+        var query = {
+            userName: username,
+        };
+        var result = await userModel.updateOne(query,data);
+        callback(null,result);
+
+    }
+    catch(err){
+        callback(err,null);
+    }
+}
+
+module.exports.deleteUser = async function(username,callback){
+    try{
+        var query = {
+            userName: username,
+        };
+        var result = await userModel.updateOne(query,{isDeleted: true});
+        callback(null,result);
+    }
+    catch(err){
+        callback(err,null);
+    }
+}
+
+module.exports.getUserByFilter = async function(filter,callback){
+    try{
+        var user = await userModel.find(filter);
+        callback(null,user);
+    }
+    catch(err){
+        callback(err,null);
+    }
+}
--- server.js
+++ server.js
@@ -4,22 +4,30 @@ const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5010;
-
+const options = {
+    extensions : ['htm', 'html', 'css', 'js', 'ico', 'jpg', 'jpeg', 'png', 'svg', 'pdf'],
+    index : ['index.html'],
+}
+app.use(express.static("public",options));
 app.get("/", function(req, res){
     // res.send("Hi Nithya");
     res.sendFile(__dirname + "/index.html");
 });
 app.get("/resume", function(req, res){
     // res.send("Hi Nithya");
-    res.sendFile(__dirname + "/resume.html");
+    res.sendFile(__dirname + "/public/resume.html");
 });
 app.get("/cssDemo", function(req, res){
     // res.send("Hi Nithya");
-    res.sendFile(__dirname + "/resumeClass.html");
+    res.sendFile(__dirname + "/public/resumeClass.html");
 });
 app.get("/cards", function(req, res){
     // res.send("Hi Nithya");
-    res.sendFile(__dirname + "/cardDemo.html");
+    res.sendFile(__dirname + "/public/cardDemo.html");
+});
+app.get("/weather", function(req, res){
+    // res.send("Hi Nithya");
+    res.sendFile(__dirname + "/public/weather.html");
 });
 mongoose.set('strictQuery',true);
 mongoose.connect(process.env.MONGO_CONNECTION_STRING,{},function(err){
@@ -31,14 +39,54 @@ mongoose.connect(process.env.MONGO_CONNECTION_STRING,{},function(err){
    {
     console.log("DB CONNECTED");
     //TODO : donot create a user if atleast 1 user exists in a table
-    userLib.createFirstUser(function(err, res){
-        if(err){
-            console.error(err);
-        }
-        else{
-            console.log(res);
-        }
-    });
+    // userLib.createFirstUser(function(err, res){
+    //     if(err){
+    //         console.error(err);
+    //     }
+    //     else{
+    //         console.log(res);
+    //     }
+    // });
+    // userLib.createUser({userName: "beingzero", yearOfGraduation: 2030},function(err,result){
+		// 	if(err){
+		// 		console.error(err);
+		// 	}
+		// 	else{
+		// 		console.log(result);
+		// 	}
+		// });
+		// userLib.updateUser(function(err,result){
+		// 	if(err){
+		// 		console.error(err);
+		// 	}
+		// 	else{
+		// 		console.log(result);
+		// 	}
+		// });
+		// userLib.deleteUser("Nithya",function(err,result){
+		// 	if(err){
+		// 		console.error(err);
+		// 	}
+		// 	else{
+		// 		console.log(result);
+		// 	}
+		// });
+		// userLib.getUserByFilter({userName: "Nithya"}, function(err,result){
+		// 	if(err){
+		// 		console.error(err);
+		// 	}
+		// 	else{
+		// 		console.log(result);
+		// 	}
+		// });
+		// userLib.getAllUsers(function(err,result){
+		// 	if(err){
+		// 		console.error(err);
+		// 	}
+		// 	else{
+		// 		console.log(result);
+		// 	}
+		// });
     app.listen(port, function(){
         console.log("Server is running on http://localhost:" + port);
     });
